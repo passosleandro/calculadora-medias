59fbdc9876e45c4e7699da388e87037130acce19
LEANDRO JOSE FERREIRA PASSOS <ferreiraepassos@hotmail.com>
2025-05-09 09:45:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./documentos/aulahtml/media/aprovado.png" alt="Emoji feliz" />';
+const imgReprovado = '<img src="./documentos/aulahtml/media/reprovado.png" alt="Emoji decepcionado" />';
+const atividades = [];
+const notas = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+    atualizaMediaFinal();
+});
+
+function adicionaLinha() {
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+}
+
+function atualizaTabela() {
+    const corpTabela = document.querySelector('tbody');
+    corpTabela.innerHTML = linhas;
+}
+
+function atualizaMediaFinal() {
+    const mediaFinal = calculaMediaFinal();
+
+    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? 'Aprovado' : 'Reprovado';
+}
+
+function calculaMediaFinal() {
+    let somaDasNotas = 0;
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i];
+    }
+
+    return somaDasNotas / notas.length;
+}
